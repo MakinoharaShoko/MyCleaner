a28fd8d8605b60d400f855cc706f3402b0913a50
Mahiru <Mahiru_@outlook.com>
2023-03-05 18:46:20 +0800

    Create clean.sh

---
+++ clean.sh
@@ -0,0 +1,18 @@
+
+# 在这里输入你的用户名
+username=Makinohara
+
+# 清理 QQ Group2
+rm -rf C:\\Users\\$username\\Documents\\Tencent Files\\2511134465\\Image\\Group2
+
+# 清理 Yarn 缓存
+rm -rf C:\\Users\\$username\\AppData\\Local\\Yarn\\Cache
+
+# 清理 pip 缓存
+rm -rf C:\\Users\\$username\\AppData\\Local\\pip\\cache
+
+# 清理 npm 缓存
+rm -rf C:\\Users\\$username\\AppData\\Local\\npm-cache
+
+# 清理 QQ Music Cache
+rm -rf C:\\Users\\$username\\AppData\\Roaming\\Tencent\\QQMusic\\QQMusicCache
